62a827ebdbae63be6f85608a794190f2e27b532b
riyabansal952 <66059749+riyabansal952@users.noreply.github.com>
2020-11-02 08:37:55 +0530

    Add files via upload

---
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+namespace ConstructorExample
+{
+
+    class Riya
+    {
+
+        int id;
+        string name;
+        Riya()
+        {
+            Console.WriteLine("Default Constructor Called");
+        }
+        Riya(String name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+     
+        public static void Main()
+        { 
+            Riya r1 = new Riya(); 
+            Riya r2 = new Riya("Riya Bansal", 1);
+            Console.WriteLine("RiyaName = " + r2.name + " and RiyaId = " + r2.id);
+          
+        }
+    }
+}
